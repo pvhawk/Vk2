300d651cbcdcdaddcd1301769da13044d65aa136
pvhawk <pvhawk87@gmail.com>
2022-04-19 22:34:31 +0600

    fix bug, add enter id, delete comment, verify photo

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 import json
-from pprint import pprint
 from progress.bar import IncrementalBar
 
 
-class VK_photo:
+class Vk_photo:
     def __init__(self, token: str):
         self.token = token
+        self.photo_base = []
 
     def get_vk_user_photo(self, owner_id: str, count_photo: int):
-        URL = 'https://api.vk.com/method/photos.get'
+        url = 'https://api.vk.com/method/photos.get'
         params = {
             'owner_id': owner_id,
             'album_id': 'profile',
@@ -18,10 +18,11 @@ class VK_photo:
             'count': str(count_photo),
             'extended': '1'
         }
-        self.photo_base = []
-        res = requests.get(URL, params=params)
-        for photo in range(count_photo):
-            self.photo_base.append({'file_name': f"{res.json()['response']['items'][photo]['likes']['count']}.jpg", 'size': res.json()['response']['items'][photo]['sizes'][-1]['type'], 'url': res.json()['response']['items'][photo]['sizes'][-1]['url']})
+
+        res = requests.get(url, params=params)
+        if 'error' not in res.json():
+            for photo in range(count_photo):
+                self.photo_base.append({'file_name': f"{res.json()['response']['items'][photo]['likes']['count']}.jpg", 'size': res.json()['response']['items'][photo]['sizes'][-1]['type'], 'url': res.json()['response']['items'][photo]['sizes'][-1]['url']})
         return self.photo_base
 
 class YaUploader:
@@ -39,13 +40,11 @@ class YaUploader:
         headers = self.get_headers()
         params = {"path": folder_name}
         response = requests.put(create_folder_url, headers=headers, params=params)
-        pprint(response.json())
         return response.json()
 
     def upload_file_to_url(self, file_list, owner_id: str):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
-        # print(len(file_list))
         bar = IncrementalBar('Downloads', max=len(file_list))
 
         for photo in file_list:
@@ -58,10 +57,9 @@ class YaUploader:
             response.raise_for_status()
             if response.status_code == 202:
                 bar.next()
-                # print("Success")
 
         bar.finish()
-
+        return
 
 
 if __name__ == '__main__':
@@ -73,15 +71,11 @@ if __name__ == '__main__':
         token_ya = file_object.read().strip()
 
     count_photo = 5
-    owner_id = '254800'
+    owner_id = input('Enter id VK user: ')
 
-    vk_photo = VK_photo(token)
-
+    vk_photo = Vk_photo(token)
     photo_base = vk_photo.get_vk_user_photo(owner_id, count_photo)
-    # pprint(photo_base)
-
     uploader = YaUploader(token_ya)
-
     uploader.create_folder(owner_id)
     result = uploader.upload_file_to_url(photo_base, owner_id)
     photo_json = []
@@ -90,9 +84,6 @@ if __name__ == '__main__':
         photo.pop("url")
         photo_json.append(photo)
 
-    # pprint(photo_json)
+
     with open('Photo_json.txt', 'w') as outfile:
         json.dump(photo_json, outfile)
-
-
-
